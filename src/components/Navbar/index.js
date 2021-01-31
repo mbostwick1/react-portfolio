81ab3019ebf74817672cca8aa02710e9669d6df3
Melanie Bostwick <mbostwick1@gmail.com>
2021-01-31 12:16:36 -0500

    fixed nav fonts issues

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,76 +5,77 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <div className="flexbox-container">
-    <nav className="navbar navbar-expand-lg navbar-light transparent">
-      <Link className="navbar-brand" to="/"  id="navbar-brand">
-        Melanie Bostwick
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item" id="navLink">
-            <Link
-              to="/About"
-              className={
-                window.location.pathname === "/About"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About
-            </Link>
-          </li>
-          <li className="nav-item" id="navLink">
-            <Link
-              to="/Portfolio"
-              className={
-                window.location.pathname === "/Portfolio"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li className="nav-item" id="navLink">
-            <Link
-              to="/Contact"
-              className={
-                window.location.pathname === "/Contact"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Contact
-            </Link>
-          </li>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light transparent">
+        <Link className="navbar-brand" to="/" id="navbar-brand">
+          Melanie Bostwick
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link
+                to="/About"
+                className={
+                  window.location.pathname === "/About"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item" id="nav-item">
+              <Link
+                to="/Portfolio"
+                className={
+                  window.location.pathname === "/Portfolio"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item" id="nav-item">
+              <Link
+                to="/Contact"
+                className={
+                  window.location.pathname === "/Contact"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Contact
+              </Link>
+            </li>
 
-          <li className="nav-item" id="navLink">
-            <Link
-            to="/Resume"
-              className={
-                window.location.pathname === "../files/MelanieBostwick_Resume.pdf"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Resume
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <li className="nav-item" id="nav-item">
+              <Link
+                to="/Resume"
+                className={
+                  window.location.pathname ===
+                  "../files/MelanieBostwick_Resume.pdf"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Resume
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
